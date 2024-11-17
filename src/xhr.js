e62bbe804e1233efedb7f2c65c46e452da7c206c
cqgsm <cqgsm@users.noreply.github.com>
2024-11-17 16:40:36 +0800

    feat: add async support for request and response hooks

--- src/xhr.js
+++ src/xhr.js
@@ -9,20 +9,24 @@
  * @param {Function} callback the callback to call when done
  * @return {Request} the xhr request that is going to be made
  */
-import videojs from 'video.js';
-import window from 'global/window';
-import {merge} from './util/vjs-compat';
+import videojs from "video.js";
+import window from "global/window";
+import { merge } from "./util/vjs-compat";
 
-const callbackWrapper = function(request, error, response, callback) {
-  const reqResponse = request.responseType === 'arraybuffer' ? request.response : request.responseText;
+const callbackWrapper = function (request, error, response, callback) {
+  const reqResponse =
+    request.responseType === "arraybuffer"
+      ? request.response
+      : request.responseText;
 
   if (!error && reqResponse) {
     request.responseTime = Date.now();
     request.roundTripTime = request.responseTime - request.requestTime;
     request.bytesReceived = reqResponse.byteLength || reqResponse.length;
     if (!request.bandwidth) {
-      request.bandwidth =
-        Math.floor((request.bytesReceived / request.roundTripTime) * 8 * 1000);
+      request.bandwidth = Math.floor(
+        (request.bytesReceived / request.roundTripTime) * 8 * 1000,
+      );
     }
   }
 
@@ -33,20 +37,24 @@ const callbackWrapper = function(request, error, response, callback) {
   // videojs.xhr now uses a specific code on the error
   // object to signal that a request has timed out instead
   // of setting a boolean on the request object
-  if (error && error.code === 'ETIMEDOUT') {
+  if (error && error.code === "ETIMEDOUT") {
     request.timedout = true;
   }
 
   // videojs.xhr no longer considers status codes outside of 200 and 0
   // (for file uris) to be errors, but the old XHR did, so emulate that
   // behavior. Status 206 may be used in response to byterange requests.
-  if (!error &&
+  if (
+    !error &&
     !request.aborted &&
     response.statusCode !== 200 &&
     response.statusCode !== 206 &&
-    response.statusCode !== 0) {
-    error = new Error('XHR Failed with a response of: ' +
-                      (request && (reqResponse || request.responseText)));
+    response.statusCode !== 0
+  ) {
+    error = new Error(
+      "XHR Failed with a response of: " +
+        (request && (reqResponse || request.responseText)),
+    );
   }
 
   callback(error, request);
@@ -59,15 +67,16 @@ const callbackWrapper = function(request, error, response, callback) {
  * @param {Object} options the request options to pass to the xhr wrapper
  * @return the callback hook function return value, the modified or new options Object.
  */
-const callAllRequestHooks = (requestSet, options) => {
+const callAllRequestHooks = async (requestSet, options) => {
   if (!requestSet || !requestSet.size) {
     return;
   }
   let newOptions = options;
 
-  requestSet.forEach((requestCallback) => {
-    newOptions = requestCallback(newOptions);
-  });
+  for (const requestCallback of requestSet) {
+    newOptions = await requestCallback(newOptions);
+  }
+
   return newOptions;
 };
 
@@ -79,55 +88,75 @@ const callAllRequestHooks = (requestSet, options) => {
  * @param {Object} error the xhr error object
  * @param {Object} response the xhr response object
  */
-const callAllResponseHooks = (responseSet, request, error, response) => {
+const callAllResponseHooks = async (responseSet, request, error, response) => {
   if (!responseSet || !responseSet.size) {
     return;
   }
-  responseSet.forEach((responseCallback) => {
-    responseCallback(request, error, response);
-  });
+  for (const responseCallback of responseSet) {
+    await responseCallback(request, error, response);
+  }
 };
 
-const xhrFactory = function() {
-  const xhr = function XhrFunction(options, callback) {
+const xhrFactory = function () {
+  const xhr = async function XhrFunction(options, callback) {
     // Add a default timeout
-    options = merge({
-      timeout: 45e3
-    }, options);
+    options = merge(
+      {
+        timeout: 45e3,
+      },
+      options,
+    );
 
     // Allow an optional user-specified function to modify the option
     // object before we construct the xhr request
     // TODO: Remove beforeRequest in the next major release.
-    const beforeRequest = XhrFunction.beforeRequest || videojs.Vhs.xhr.beforeRequest;
+    const beforeRequest =
+      XhrFunction.beforeRequest || videojs.Vhs.xhr.beforeRequest;
     // onRequest and onResponse hooks as a Set, at either the player or global level.
     // TODO: new Set added here for beforeRequest alias. Remove this when beforeRequest is removed.
-    const _requestCallbackSet = XhrFunction._requestCallbackSet || videojs.Vhs.xhr._requestCallbackSet || new Set();
-    const _responseCallbackSet = XhrFunction._responseCallbackSet || videojs.Vhs.xhr._responseCallbackSet;
+    const _requestCallbackSet =
+      XhrFunction._requestCallbackSet ||
+      videojs.Vhs.xhr._requestCallbackSet ||
+      new Set();
+    const _responseCallbackSet =
+      XhrFunction._responseCallbackSet || videojs.Vhs.xhr._responseCallbackSet;
 
-    if (beforeRequest && typeof beforeRequest === 'function') {
-      videojs.log.warn('beforeRequest is deprecated, use onRequest instead.');
+    if (beforeRequest && typeof beforeRequest === "function") {
+      videojs.log.warn("beforeRequest is deprecated, use onRequest instead.");
       _requestCallbackSet.add(beforeRequest);
     }
 
     // Use the standard videojs.xhr() method unless `videojs.Vhs.xhr` has been overriden
     // TODO: switch back to videojs.Vhs.xhr.name === 'XhrFunction' when we drop IE11
-    const xhrMethod = videojs.Vhs.xhr.original === true ? videojs.xhr : videojs.Vhs.xhr;
+    const xhrMethod =
+      videojs.Vhs.xhr.original === true ? videojs.xhr : videojs.Vhs.xhr;
 
     // call all registered onRequest hooks, assign new options.
-    const beforeRequestOptions = callAllRequestHooks(_requestCallbackSet, options);
+    const beforeRequestOptions = await callAllRequestHooks(
+      _requestCallbackSet,
+      options,
+    );
 
     // Remove the beforeRequest function from the hooks set so stale beforeRequest functions are not called.
     _requestCallbackSet.delete(beforeRequest);
 
     // xhrMethod will call XMLHttpRequest.open and XMLHttpRequest.send
-    const request = xhrMethod(beforeRequestOptions || options, function(error, response) {
-      // call all registered onResponse hooks
-      callAllResponseHooks(_responseCallbackSet, request, error, response);
-      return callbackWrapper(request, error, response, callback);
-    });
+    const request = xhrMethod(
+      beforeRequestOptions || options,
+      async function (error, response) {
+        // call all registered onResponse hooks
+        await callAllResponseHooks(
+          _responseCallbackSet,
+          request,
+          error,
+          response,
+        );
+        return callbackWrapper(request, error, response, callback);
+      },
+    );
     const originalAbort = request.abort;
 
-    request.abort = function() {
+    request.abort = function () {
       request.aborted = true;
       return originalAbort.apply(request, arguments);
     };
@@ -149,19 +178,25 @@ const xhrFactory = function() {
  * @param {Object} byterange - an object with two values defining the start and end
  *                             of a byte-range
  */
-export const byterangeStr = function(byterange) {
+export const byterangeStr = function (byterange) {
   // `byterangeEnd` is one less than `offset + length` because the HTTP range
   // header uses inclusive ranges
   let byterangeEnd;
   const byterangeStart = byterange.offset;
 
-  if (typeof byterange.offset === 'bigint' || typeof byterange.length === 'bigint') {
-    byterangeEnd = window.BigInt(byterange.offset) + window.BigInt(byterange.length) - window.BigInt(1);
+  if (
+    typeof byterange.offset === "bigint" ||
+    typeof byterange.length === "bigint"
+  ) {
+    byterangeEnd =
+      window.BigInt(byterange.offset) +
+      window.BigInt(byterange.length) -
+      window.BigInt(1);
   } else {
     byterangeEnd = byterange.offset + byterange.length - 1;
   }
 
-  return 'bytes=' + byterangeStart + '-' + byterangeEnd;
+  return "bytes=" + byterangeStart + "-" + byterangeEnd;
 };
 
 /**
@@ -170,7 +205,7 @@ export const byterangeStr = function(byterange) {
  * @param {Object} segment - a simplified copy of the segmentInfo object
  *                           from SegmentLoader
  */
-const segmentXhrHeaders = function(segment) {
+const segmentXhrHeaders = function (segment) {
   const headers = {};
 
   if (segment.byterange) {
@@ -179,6 +214,6 @@ const segmentXhrHeaders = function(segment) {
   return headers;
 };
 
-export {segmentXhrHeaders, callbackWrapper, xhrFactory};
+export { segmentXhrHeaders, callbackWrapper, xhrFactory };
 
 export default xhrFactory;
